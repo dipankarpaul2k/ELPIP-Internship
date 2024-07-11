da3cbaee56b79edb2f3c53692d96e8ac27ace0d7
dipankarpaul2k <dipankarpaul2k@gmail.com>
2024-07-11 23:55:35 +0530

    change mongodb uri from local to atlas

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -4,7 +4,9 @@ const MONGO_URI = String(process.env.MONGO_URI);
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(MONGO_URI);
+    await mongoose.connect(MONGO_URI, {
+      dbName: "taskshare"
+    });
     console.log("MongoDB connected");
   } catch (error) {
     console.error("Error connecting to MongoDB:", error.message);
