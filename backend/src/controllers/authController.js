5da6a0e0da377353003a64f81a59cc6e7a5850c6
dipankarpaul2k <dipankarpaul2k@gmail.com>
2024-07-12 11:01:46 +0530

    add domain in cookies

--- backend/src/controllers/authController.js
+++ backend/src/controllers/authController.js
@@ -40,6 +40,7 @@ export const register = async (req, res) => {
         sameSite: "none",
         secure: true,
         maxAge: 7 * 24 * 60 * 60 * 1000,
+        domain: '.render.com'
       });
       res.json({ msg: "Registration successful", token, user });
     });
@@ -79,6 +80,7 @@ export const login = async (req, res) => {
         sameSite: "none",
         secure: true,
         maxAge: 7 * 24 * 60 * 60 * 1000,
+        domain: '.render.com'
       });
       res.json({ msg: "Login successful", token, user });
     });
