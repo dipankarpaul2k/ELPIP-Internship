604c93e7859e235f1397fe120eb0915f5bd35ac6
dipankarpaul2k <dipankarpaul2k@gmail.com>
2024-07-12 21:04:44 +0530

    add completion status in task view modal

--- frontend/src/components/Modals/TaskViewModal.jsx
+++ frontend/src/components/Modals/TaskViewModal.jsx
@@ -9,18 +9,22 @@ export default function TaskViewModal({ isOpened, onClose, task }) {
         size={"lg"}
         centered
         title={
-          <Text
-            fw={500}
-            fz={{ base: "h4", sm: "h3" }}
-          >
+          <Text fw={500} fz={{ base: "h4", sm: "h3" }}>
             {task.title}
           </Text>
         }
       >
+        {/* Task description */}
         <Text my={"sm"}>{task.description}</Text>
+        {/* Task status */}
+        <Divider my={"sm"} />
+        <Text fw={500}>Status</Text>
+        <Text>{task?.completed ? "Completed" : "Pending"}</Text>
+        {/* Task owner */}
         <Divider my={"sm"} />
         <Text fw={500}>Owner</Text>
         <Text>{task?.owner?.username}</Text>
+        {/* Task shared with */}
         <Divider my={"sm"} />
         <Text fw={500}>Shared with</Text>
         <Group gap={5}>
